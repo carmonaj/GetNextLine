ce4783b6e7ffb82d4ea409d6ae3d382ca06e0339
Jonathan Carmona <carmona.johnny@gmail.com>
2016-11-16 13:18:05 -0800

    backup

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcarmona <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/13 14:47:02 by jcarmona          #+#    #+#             */
+/*   Updated: 2016/11/13 14:47:03 by jcarmona         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+
+# include <stdlib.h>
+
+int		get_next_line(const int fd, char **line);
+
+#endif
