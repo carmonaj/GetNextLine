6c85eab996bd8a836774725ea2b08ce584403320
Jonathan Carmona <carmona.johnny@gmail.com>
2016-11-18 18:54:15 -0800

    backup

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,12 @@
 /*   By: jcarmona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/13 14:46:48 by jcarmona          #+#    #+#             */
-/*   Updated: 2016/11/17 19:25:11 by jcarmona         ###   ########.fr       */
+/*   Updated: 2016/11/18 18:46:20 by jcarmona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 char	*build_str(t_list *node)
 {
@@ -24,7 +25,7 @@ char	*build_str(t_list *node)
 		if (node->content)
 		{
 			if (!s)
-				s = strdup(node->content);
+				s = ft_strdup(node->content);
 			else
 			{
 				tmp = s;
@@ -37,13 +38,13 @@ char	*build_str(t_list *node)
 	return (s);
 }
 
-int		nl_check(char **s, t_list *result)
+int		nl_check(char **s, t_list *result, int fd)
 {
 	char	*cpy;
 	int		i;
 
 	i = 0;
-	if (strchr(*s, '\n'))
+	if (ft_strchr(*s, '\n'))
 	{
 		cpy = *s;
 		while ((*s)[i] != '\n')
@@ -52,7 +53,11 @@ int		nl_check(char **s, t_list *result)
 		if (cpy[++i])
 			result->content = ft_strdup(&cpy[i]);
 		else
+		{
 			result->content = 0;
+			if (fd == 0)
+				return (0);
+		}
 		return (1);
 	}
 	return (0);
@@ -62,24 +67,40 @@ int		get_next_line(const int fd, char **line)
 {
 	static	t_list	result;
 	t_list			*node;
+	int				ret;
 
-	if (!fd)
+	if (fd < 0 || !line) 
 		return (-1);
+
 	node = ft_lstnew(ft_strnew(BUFF_SIZE), BUFF_SIZE);
 	if (result.content)
 	{
 		node->content = ft_strdup(result.content);
-		if (nl_check((char**)&node->content, &result))
+		if (nl_check((char**)&node->content, &result, fd))
 			if ((*line = build_str(node)))
 				return (1);
 		ft_lstadd(&node, ft_lstnew(ft_strnew(BUFF_SIZE), BUFF_SIZE));
 	}
-	while ((read(fd, node->content, BUFF_SIZE)) > 0)
+	while ((ret = read(fd, node->content, BUFF_SIZE)) > 0)
 	{
-		if (nl_check((char**)&node->content, &result))
+		if (nl_check((char**)&node->content, &result, fd))
 			if ((*line = build_str(node)))
 				return (1);
 		ft_lstadd(&node, ft_lstnew(ft_strnew(BUFF_SIZE), BUFF_SIZE));
 	}
+	if (ret < 0)
+		return (-1);
+
+	if (node->next)
+	{
+		*line = build_str(node);
+		if (result.content)
+			result.content = 0;
+		if (node->next)
+			node->next = 0;
+		return (1);
+	}
+	else
+		*line = 0;
 	return (0);
 }
