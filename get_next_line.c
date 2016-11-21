cdea98b9973fe1727d86cd85cf69b73a12619cc1
Jonathan Carmona <carmona.johnny@gmail.com>
2016-11-20 18:39:53 -0800

    backup

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jcarmona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/13 14:46:48 by jcarmona          #+#    #+#             */
-/*   Updated: 2016/11/20 05:54:11 by jcarmona         ###   ########.fr       */
+/*   Updated: 2016/11/20 18:39:45 by jcarmona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 char	*build_str(t_list *node)
 {
+	t_list	*t_tmp;
 	char	*tmp;
 	char	*s;
 
@@ -29,14 +30,18 @@ char	*build_str(t_list *node)
 			{
 				tmp = s;
 				s = ft_strjoin(node->content, s);
+				free(tmp);
 			}
 		}
+		t_tmp = node;
 		node = node->next;
+		free(t_tmp);
 	}
 	return (s);
 }
 
-int		nl_check(char **s, t_list *result)
+
+/*int		nl_check(char **s, t_list *result)
 {
 	char	*cpy;
 	int		i;
@@ -50,17 +55,39 @@ int		nl_check(char **s, t_list *result)
 			i++;
 		*s = ft_strsub(*s, 0, i);
 		if ((ft_strlen(cpy += i+1)) > 0)
-		{
-			//result->content = ft_strdup(&cpy[i]);
 			result->content = ft_strdup(cpy);
-			//printf("\n[%s]\n", result->content);
+		else
+			result->content = 0;
+		return (1);
+		
+	}
+	return (0);
+}*/
+
+int		nl_check(char **s, t_list *result)
+{
+	char	*cpy;
+	char	*cpy2;
+	int		i;
+
+	i = 0;
+	cpy2 = *s;
+	if (ft_strchr(*s, '\n'))
+	{
+		cpy = *s;
+		while ((*s)[i] != '\n')
+			i++;
+		*s = ft_strsub(*s, 0, i);
+		if ((ft_strlen(cpy += i+1)) > 0)
+		{
+			result->content = ft_strdup(cpy);
+	//		printf("[%s:%zu]\n", *s, ft_strlen(*s));
 		}
 		else
-		{
 			result->content = 0;
-			//printf("its empty\n");
-		}
+		free(cpy2);
 		return (1);
+		
 	}
 	return (0);
 }
@@ -71,16 +98,15 @@ int		get_next_line(const int fd, char **line)
 	static	t_list	result;
 	char			buff[BUFF_SIZE+1];
 	t_list			*node;
+	//char			*cpy;
+	char			*cpy2;
 	int				ret;
 
 	ft_memset(buff, 0, BUFF_SIZE+1);
 
 	/* if fd is negative or line pointer is null */
 	if (fd < 0 || !line) 
-	{
-		//printf("error\n");
 		return (-1);
-	}
 
 	/* create a new node with string of buff_size*/
 	node = ft_lstnew(0, 0);
@@ -88,22 +114,18 @@ int		get_next_line(const int fd, char **line)
 	/* if string exists already */
 	if (result.content)
 	{
-		//printf("<<[%s]>>", result.content);
-		//printf("<<%s>>", result.content);
 		/* assign the string to the new node */
 		node->content = ft_strdup(result.content);
+		free(result.content);
 		result.content = 0;
+
 		/* check if there is a newline in this string */
 		if (nl_check((char**)&node->content, &result))
-
 			/* if there is, build string & return 1 */
 			if ((*line = build_str(node)))
-			{
-//				printf("(c)");
 				return (1);
-			}
+
 		/* if there is no newline, create another node and add to the beginning of list */
-		//ft_lstadd(&node, ft_lstnew(ft_strnew(BUFF_SIZE), BUFF_SIZE));
 		ft_lstadd(&node, ft_lstnew(0, 0));
 	}
 
@@ -111,48 +133,36 @@ int		get_next_line(const int fd, char **line)
 	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		node->content = ft_strdup(buff);
-		ft_memset(buff, 0, BUFF_SIZE);
-		/*if (ret < BUFF_SIZE)
-		{
-			*line = build_str(node);
-			return (1);
-		}*/
+		ft_memset(buff, 0, BUFF_SIZE+1);
 
 		/* check if there is a newline in this string */
 		if (nl_check((char**)&node->content, &result) || ret < BUFF_SIZE)
-
+		{
+			cpy2 = node->content;
+			//printf("[%s:%zu]\n", node->content, ft_strlen(node->content));
 			/* if there is, build string & return 1 */
 			if ((*line = build_str(node)))
 			{
-//				printf("(b)");
-				//printf("[[LINE:%s]]\n", *line);
+				free(cpy2);
 				return (1);
 			}
+		}
 
 		/* if there is no newline, create another node and add to the beginning of list */
-		//ft_lstadd(&node, ft_lstnew(ft_strnew(BUFF_SIZE), BUFF_SIZE));
 		ft_lstadd(&node, ft_lstnew(0, 0)); 
 	}
 
 	/* if you can't read from file/ if invalid */
 	if (ret < 0)
-	{
-		//printf("error\n");
-		return (-1);
-	}
+			return (-1);
 
 	if (node->next)
 	{
-		//printf("LOL\n");
 		*line = build_str(node);
-//		printf("(a)");
 		return (1);
-	/*	printf("[result.content:%s]", result.content);
-		result.content = 0;
-		printf("<foobar>");
-		return (0);*/
 	}
-	
-//	printf("(d)");
+
+	free(node->content);
+	free(node);
 	return (0);
 }
